efd9c204426e0aa1801186c47f9c07a962af0e65
Pavel <neprim1@yandex.ru>
2021-07-17 20:20:27 +0300

    Add song veil

--- DiscordBot.py
+++ DiscordBot.py
@@ -38,8 +38,9 @@ list_prew = []
 music_welcome = [[284610292095123456,"music/Dungeon master.mp3"],[209443383385522176,"music/y2mate.mp3"],
                  [217236953324584960,"music/Boy next door.mp3"],[400709228270059531,"music/Fucking slaves get your ass back here.mp3"],
                  [193425328083828736,"music/Stick your finger in my ass.mp3"],[196682643767689218,"music/FUCK YOU.mp3"],
-                 [310730616926896128,"music/I played with his dick.mp3"],[478605922285912074,"music/Do you like watching me.mp3"],
-                 [306678386800066562,"music/WOO.mp3"],[429219492279877642,"music/Fisting is 300 $.mp3"]]
+                 [310730616926896128,"music/Fisting is 300 $.mp3"],[478605922285912074,"music/Do you like watching me.mp3"],
+                 [306678386800066562,"music/WOO.mp3"],[429219492279877642,"music/Fisting is 300 $.mp3"],
+                 [429219492279877642,"music/Oh shit iam sorry.mp3"]]
 global_xur = [('16.07.2021', '19:05')]
 
 @client.event
